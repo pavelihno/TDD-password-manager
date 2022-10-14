9192c4dd8faba731583d2dbe59acff2a4d7bcc34
pavelihno <crazzyyellow02@gmail.com>
2022-10-14 11:37:02 +0300

    создан тестовый класс, пустые методы для тестирования

---
+++ tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class PasswordManagerTest(unittest.TestCase):
+
+    def setUp(self):
+        key = 'Ar1Vider4I'
+
+
+    def test_saving_password_by_new_login(self):
+        pass
+
+    def test_saving_password_by_existing_login(self):
+        pass
+
+    def test_deleting_password_by_login(self):
+        pass
+
+    def test_getting_password_by_non_existing_login(self):
+        pass
+
+    def test_getting_password_by_existing_login(self):
+        pass
+
+    def test_wrong_password_manager_key_entered(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
